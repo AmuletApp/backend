89fac3b8a06f2932102948c52930a171f5cded79
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-01-30 10:29:06 -0800

    refactor(PublishRequest): implicit typing

--- src/main/kotlin/com/github/redditvanced/database/PublishRequest.kt
+++ src/main/kotlin/com/github/redditvanced/database/PublishRequest.kt
@@ -1,13 +1,12 @@
 package com.github.redditvanced.database
 
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.Column
 
 object PublishRequest : IntIdTable("publish_requests") {
-	val owner: Column<String> = varchar("owner", 39, "NOCASE")
-	val repo: Column<String> = varchar("repo", 100, "NOCASE")
-	val plugin: Column<String> = varchar("plugin", 40, "NOCASE")
-	val targetCommit: Column<String> = varchar("target_commit", 40)
-	val messageId: Column<Long?> = long("message_id").nullable()
-	val updates: Column<Int> = integer("updates").default(0)
+	val owner = varchar("owner", 39, "NOCASE")
+	val repo = varchar("repo", 100, "NOCASE")
+	val plugin = varchar("plugin", 40, "NOCASE")
+	val targetCommit = varchar("target_commit", 40)
+	val messageId = long("message_id").nullable()
+	val updates = integer("updates").default(0)
 }
